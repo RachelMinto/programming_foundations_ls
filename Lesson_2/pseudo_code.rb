867907dfb360cdf43933067600b6ca050d0a8c13
Rachel Minto <rachel.j.minto@gmail.com>
2016-04-29 17:17:39 +0000

    Lesson_2 first commit of exercises.

---
+++ Lesson_2/pseudo_code.rb
@@ -0,0 +1,48 @@
+
+=begin
+method sum
+Given two integers
+Implicitly return integer1 + integer2
+end
+=end
+
+=begin
+#given two integers
+START
+add integer1 to integer2
+END
+
+
+=begin
+method that takes an array of strings, and returns a string that is all those strings concatenated together
+Given an array of strings
+Set result to empty string
+Iterate over each string in array
+    -Concatenate to result
+return result
+=end
+
+=begin
+Given an array of strings
+SET result = ""
+EACH string in array
+    result + string
+RETURN result
+END
+
+=end
+
+=begin
+method that takes an array of integers, and returns a new array with every other element
+given an array of integers
+iterate over each string in array using .select to apply block of code
+    -select every other. Use .odd??
+implicit return
+end
+
+=begin
+#Given an array of integers
+SELECT strings in array
+    if index of string ODD?
+END
+=end
