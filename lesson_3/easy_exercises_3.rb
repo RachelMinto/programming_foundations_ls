6760ba99c55608a8186d77c5dc1e6f0a44313606
Rachel Minto <rachel.j.minto@gmail.com>
2016-05-03 18:50:10 +0000

    Finished easy and started medium.

---
+++ lesson_3/easy_exercises_3.rb
@@ -0,0 +1,12 @@
+puts 'Q. 1. flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)'
+
+puts 'Q. 2 flintstones.push("Dino"). Better: flintstones << "Dino"'
+
+puts 'Q. 3 flintstones.push("Dino", "Hoppy")'
+
+puts 'Q 4 advice.slice!(0, 39). Bonus: You would return the last part but leave advice unmodified.'
+
+puts 'Q. 5 statement.count "t"'
+
+puts 'Q 6 title.center(40)'
+
---
+++ lesson_3/medium_exercises_1.rb
@@ -0,0 +1,2 @@
+
+puts 'Q 1 10.times { |i| puts ""*i + "The Flintstones Rock!" }  '
