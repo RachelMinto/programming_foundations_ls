e8573a69ec488ca3eb6e556222b08fdc7221a026
Rachel Minto <rachel.j.minto@gmail.com>
2016-05-10 21:00:37 -0400

    Refactored TTT.

--- lesson_4/tic_tac_toe.rb
+++ lesson_4/tic_tac_toe.rb
@@ -1,6 +1,4 @@
 
-require 'pry'
-
 INITIAL_MARKER = ' '.freeze
 PLAYER_MARKER = 'X'.freeze
 COMPUTER_MARKER = 'O'.freeze
@@ -18,10 +16,10 @@ def prompt(string)
 end
 
 # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-def display_board(brd, player_score, computer_score)
+def display_board(brd, score)
   puts "You're an #{PLAYER_MARKER}. The Computer is a #{COMPUTER_MARKER}."
-  puts "You have won #{player_score.last} games and\
-  the Computer has won #{computer_score.last}"
+  puts "You have won #{score[:player]} games and\
+ the Computer has won #{score[:computer]}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -48,10 +46,10 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
-def immediate_threat?(brd, marker)
+def immediate_threat(brd, marker)
   square = nil
 
-  WINNING_LINES.each do |line| 
+  WINNING_LINES.each do |line|
     square = find_at_risk_square(line, brd, marker)
     break if square
   end
@@ -61,8 +59,6 @@ end
 def find_at_risk_square(line, board, marker)
   if board.values_at(*line).count(marker) == 2
     board.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
-  else
-    nil
   end
 end
 
@@ -71,7 +67,7 @@ def find_better_open_square(brd, choices, marker)
   best_choices = []
   choices.each do |open_square|
     test_choice_on_board[open_square] = marker
-    best_choices.push(open_square) if immediate_threat?(test_choice_on_board, marker)
+    best_choices.push(open_square) if immediate_threat(test_choice_on_board, marker)
     test_choice_on_board = brd.clone
   end
   best_choices.sample
@@ -79,7 +75,7 @@ end
 
 def find_odd_open_squares(brd, marker)
   choices = []
-  brd.each_with_index do |index, value| 
+  brd.each_with_index do |index, _|
     choices.push(index[0]) if index[1] == INITIAL_MARKER && index[0].odd?
   end
 
@@ -88,7 +84,6 @@ def find_odd_open_squares(brd, marker)
   else
     choices.sample
   end
-
 end
 
 def player_places_piece!(brd)
@@ -103,34 +98,32 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = if immediate_threat?(brd, COMPUTER_MARKER)
-             immediate_threat?(brd, COMPUTER_MARKER)
-           elsif immediate_threat?(brd, PLAYER_MARKER)
-             immediate_threat?(brd, PLAYER_MARKER)
+  square = if immediate_threat(brd, COMPUTER_MARKER)
+             immediate_threat(brd, COMPUTER_MARKER)
+           elsif immediate_threat(brd, PLAYER_MARKER)
+             immediate_threat(brd, PLAYER_MARKER)
            elsif brd[5] == INITIAL_MARKER
              5
            elsif find_odd_open_squares(brd, COMPUTER_MARKER)
-             find_odd_open_squares(brd, COMPUTER_MARKER) 
+             find_odd_open_squares(brd, COMPUTER_MARKER)
            else
              empty_squares(brd).sample
-  end
+           end
   brd[square] = COMPUTER_MARKER
 end
 
-def validate_answer(ans)
+def validate_answer
   loop do
-    break if ans.downcase.start_with?('y', 'n')
-    prompt "Please respond with either Y or N."
     ans = gets.chomp
+    return ans if ans.downcase.start_with?('y', 'n')
+    prompt "Please respond with either Y or N."
   end
-  ans
 end
 
 def get_starter(starter)
   if starter == 'choose'
     prompt "Would you like to start? (Y or N)."
-    ans = gets.chomp
-    final_ans = validate_answer(ans)
+    final_ans = validate_answer
     return 'Player' if final_ans.downcase.start_with?('y')
     'Computer'
   else
@@ -150,10 +143,10 @@ def place_piece!(board, current_player)
   end
 end
 
-def take_turns(current_player, board, player_score, computer_score)
+def take_turns(current_player, board, score)
   loop do
-    system 'clear' or system 'cls'
-    display_board(board, player_score, computer_score)
+    system('clear') || system('cls')
+    display_board(board, score)
     place_piece!(board, current_player)
     current_player = alternate_player(current_player)
     break if someone_won?(board) || board_full?(board)
@@ -179,11 +172,11 @@ def detect_winner(brd)
   nil
 end
 
-def update_score(brd, player_score, computer_score)
+def update_score(brd, score)
   if detect_winner(brd) == 'Player'
-    player_score << player_score.last.succ
+    score[:player] += 1
   elsif detect_winner(brd) == 'Computer'
-    computer_score << computer_score.last.succ
+    score[:computer] += 1
   end
 end
 
@@ -197,38 +190,35 @@ end
 prompt "Welcome to Tic Tac Toe. Each match will be best out of #{WINNING_SCORE}."
 
 loop do
-  player_score = [0]
-  computer_score = [0]
+  score = { player: 0, computer: 0 }
   loop do
     board = initialize_board
     who_starts = get_starter(STARTER)
-    take_turns(who_starts, board, player_score, computer_score)
+    take_turns(who_starts, board, score)
 
     if someone_won?(board)
-      update_score(board, player_score, computer_score)
-      system 'clear' or system 'cls'
+      update_score(board, score)
+      system('clear') || system('cls')
       prompt "#{detect_winner(board)} won!"
     else
-      system 'clear' or system 'cls'
+      system('clear') || system('cls')
       prompt "It's a tie!"
     end
 
-    break if player_score.last == WINNING_SCORE || computer_score.last == WINNING_SCORE
-    display_board(board, player_score, computer_score)
+    break if score[:player] == WINNING_SCORE || score[:computer] == WINNING_SCORE
+    display_board(board, score)
   end
 
-  if player_score.last == WINNING_SCORE
-    prompt "Congratulations! You have won #{WINNING_SCORE} matches\ 
+  if score[:player] == WINNING_SCORE
+    prompt "Congratulations! You have won #{WINNING_SCORE} matches\
  against the computer."
     prompt "Would you like to play another match? (y or n)"
-    ans = gets.chomp
-    final_ans = validate_answer(ans)
+    final_ans = validate_answer
     break if final_ans.downcase.start_with?('n')
-  elsif computer_score.last == WINNING_SCORE
+  elsif score[:computer] == WINNING_SCORE
     prompt "Too bad. The computer has won this match.\
-    Would you like to play again? (y or n)."
-    ans = gets.chomp
-    final_ans = validate_answer(ans)
+ Would you like to play again? (y or n)."
+    final_ans = validate_answer
     break if final_ans.downcase.start_with?('n')
   end
 end
